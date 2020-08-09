5359321521e64d0191aa5a1be24adce6fbbfebac
Vitalii <web.vitaly@gmail.com>
2020-08-09 17:48:10 -0400

    Added NotFound component
    * Added redirect of any URL to NotFound compoanent
    * Moved Nav compoanent to a proper location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { FormReactiveComponent } from './components/form-reactive/form-reactive.component';
 import { FormTemplateComponent } from './components/form-template/form-template.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: FormReactiveComponent },
   { path: 'form-template', component: FormTemplateComponent },
-  { path: 'form-reactive', component: FormReactiveComponent }
+  { path: 'form-reactive', component: FormReactiveComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormReactiveComponent } from './components/form-reactive/form-reactive.component';
 import { FormTemplateComponent } from './components/form-template/form-template.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './components/nav/nav.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormReactiveComponent,
     FormTemplateComponent,
-    NavComponent
+    NavComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
