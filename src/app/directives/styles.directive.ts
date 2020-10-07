b8b504b66069609f76831ca5393aa735b5ef20ab
Vitalii <web.vitaly@gmail.com>
2020-10-07 19:31:59 -0400

    Fixed demo directive to fallback to defaults

--- src/app/directives/styles.directive.ts
+++ src/app/directives/styles.directive.ts
@@ -4,7 +4,8 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
   selector: '[appStyles]'
 })
 export class StylesDirective {
-  @Input('appStyles') styles: {color: string, border: string} = {color: 'green', border: '1px solid blue'};
+  private stylesDefault = {color: 'green', border: '1px solid blue'};
+  @Input('appStyles') styles: {color: string, border: string};
 
   constructor(private elemRef: ElementRef, private renderer: Renderer2) { }
 
@@ -13,14 +14,18 @@ export class StylesDirective {
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.renderer.setStyle(this.elemRef.nativeElement, 'color', this.styles.color);
-    this.renderer.setStyle(this.elemRef.nativeElement, 'border', this.styles.border);
-    console.log(this.styles.color, this.styles.border)
+    //this.renderer.setStyle(this.elemRef.nativeElement, 'color', this.styles.color);
+    this.elemRef.nativeElement.style.color = this.styles?.color || this.stylesDefault.color;
+    //this.renderer.setStyle(this.elemRef.nativeElement, 'border', this.styles.border);
+    this.elemRef.nativeElement.style.border = this.styles?.border || this.stylesDefault.border;
+
   }
 
   @HostListener('mouseleave') onLeave() {
-    this.renderer.setStyle(this.elemRef.nativeElement, 'color', null);
-    this.renderer.setStyle(this.elemRef.nativeElement, 'border', null);
+    //this.renderer.setStyle(this.elemRef.nativeElement, 'color', null);
+    this.elemRef.nativeElement.style.color = null;
+    //this.renderer.setStyle(this.elemRef.nativeElement, 'border', null);
+    this.elemRef.nativeElement.style.border = null;
   }
 
 }
