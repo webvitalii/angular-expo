5128ac8b7dae3171023b53f2b6a6391224601b47
Vitalii <web.vitaly@gmail.com>
2020-11-06 22:45:02 -0500

    Updated Angular and Bootstrap
    ng update @angular/cli @angular/core --allow-dirty
    npm install bootstrap@5.0.0-alpha2 --save

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-demos app is running!');
+    expect(page.getTitleText()).toEqual('angular-expo app is running!');
   });
 
   afterEach(async () => {
