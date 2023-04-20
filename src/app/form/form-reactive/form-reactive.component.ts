b48d85d51f683e3d2f48c2e7d2cf696e09219e62
Vitalii <vitalii.skorokhod@gmail.com>
2023-04-19 21:05:05 -0400

    Updated reactive form page

--- src/app/form/form-reactive/form-reactive.component.ts
+++ src/app/form/form-reactive/form-reactive.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 import { CustomValidators } from './custom.validators';
 
 @Component({
@@ -7,13 +8,16 @@ import { CustomValidators } from './custom.validators';
   templateUrl: './form-reactive.component.html',
   styleUrls: ['./form-reactive.component.scss']
 })
-export class FormReactiveComponent implements OnInit {
+export class FormReactiveComponent implements OnInit, OnDestroy {
   form: FormGroup;
+
+  private unsubscribe$ = new Subject<void>();
 
   private formDefaults = {
     username: 'default-username',
     email: '',
-    gender: 'male'
+    gender: 'male',
+    agreeTerms: false
   };
 
   genderList = {
@@ -34,9 +38,44 @@ export class FormReactiveComponent implements OnInit {
         CustomValidators.forbiddenUsernames
       ]),
       email: new FormControl(null, [Validators.required, Validators.email], [CustomValidators.forbiddenEmailAsync]),
-      gender: new FormControl(null, Validators.required)
+      gender: new FormControl(null, Validators.required),
+      agreeTerms: new FormControl(null, Validators.requiredTrue)
     });
     this.form.patchValue(this.formDefaults);
+
+    this.trackFormChanges();
+  }
+
+  trackFormChanges() {
+    this.form.valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe(() => {
+      const inputsWithErrors: string[] = [];
+  
+      Object.keys(this.form.controls).forEach((key) => {
+        const control = this.form.get(key);
+        if (control.invalid) {
+          inputsWithErrors.push(`${key}: ${JSON.stringify(control.errors)}`);
+        }
+      });
+  
+      if (inputsWithErrors.length > 0) {
+        console.log('Inputs with errors:', inputsWithErrors);
+      }
+    });
+  }
+
+  collectFormErrors(): object {
+    let errors = {};
+  
+    for (const controlName in this.form.controls) {
+      const control = this.form.get(controlName);
+      const controlErrors = control.errors;
+  
+      if (control.invalid && controlErrors) {
+        errors[controlName] = controlErrors;
+      }
+    }
+  
+    return errors;
   }
 
   onSubmit() {
@@ -46,5 +85,10 @@ export class FormReactiveComponent implements OnInit {
     console.log(this.form);
     console.log(this.form.value);
     this.form.reset(this.formDefaults);
+  }
+  
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
