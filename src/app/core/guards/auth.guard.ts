a035f807a1123669d194b5b1eebf5faf890a9de6
Vitalii <vitalii.skorokhod@gmail.com>
2023-07-09 16:52:54 -0400

    ng update @angular/cli @angular/core --allow-dirty --force
    >ng update @angular/cli @angular/core --allow-dirty --force
    The installed Angular CLI version is outdated.
    Installing a temporary Angular CLI versioned 16.1.4 to perform the update.
    √ Packages successfully installed.
    Workspace extension with invalid name (defaultProject) found.
    Using package manager: npm
    Collecting installed dependencies...
    Found 40 dependencies.
    Fetching dependency metadata from registry...
                      Package "@angular-eslint/schematics" has an incompatible peer dependency to "@angular/cli" (requires ">= 15.0.0 < 16.0.0", would install "16.1.4").
        Updating package.json with dependency @angular-devkit/build-angular @ "16.1.4" (was "15.2.8")...
        Updating package.json with dependency @angular/cli @ "16.1.4" (was "15.2.8")...
        Updating package.json with dependency @angular/compiler-cli @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency typescript @ "5.1.6" (was "4.8.4")...
        Updating package.json with dependency @angular/animations @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency @angular/common @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency @angular/compiler @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency @angular/core @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency @angular/forms @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency @angular/platform-browser @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency @angular/platform-browser-dynamic @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency @angular/router @ "16.1.4" (was "15.2.9")...
        Updating package.json with dependency zone.js @ "0.13.1" (was "0.12.0")...
    UPDATE package.json (1755 bytes)
    √ Packages successfully installed.
    ** Executing migrations of package '@angular/cli' **
    
    > Remove 'defaultProject' option from workspace configuration.
      The project to use will be determined from the current working directory.
    Workspace extension with invalid name (defaultProject) found.
    UPDATE angular.json (5099 bytes)
      Migration completed (1 file modified).
    
    > Replace removed 'defaultCollection' option in workspace configuration with 'schematicCollections'.
      Migration completed (No changes made).
    
    > Update the '@angular-devkit/build-angular:server' builder configuration to disable 'buildOptimizer' for non optimized builds.
      Migration completed (No changes made).
    
    ** Executing migrations of package '@angular/core' **
    
    > In Angular version 15.2, the guard and resolver interfaces (CanActivate, Resolve, etc) were deprecated.
      This migration removes imports and 'implements' clauses that contain them.
    UPDATE src/app/core/guards/auth.guard.ts (730 bytes)
      Migration completed (1 file modified).
    
    > As of Angular v16, the `moduleId` property of `@Component` is deprecated as it no longer has any effect.
      Migration completed (No changes made).

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
